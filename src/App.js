74fd54b84511ef7133e622621b15b7b2f0eaf656
Mike Yee <mikeyee@ud.hk>
2021-11-21 15:38:13 +0800

    add buttons to switch exchange

--- src/App.js
+++ src/App.js
@@ -1,73 +1,46 @@
 import './App.css';
 import { createClient } from 'urql'
-import { useEffect, useState } from 'react'
+import React, { Component } from 'react';
+// import { useEffect, useState, Component } from 'react'
 import { CSVLink } from "react-csv";
 
-const first = 10 // 1000
+const first = 5 // 1000
 const endQuery = 10 //4000
 const initialSkip = 0
 const csvFilename = 'swap-report-'+Date.now()+'.csv'
+
+const apiExchangeUrl = {
+  "uniswap": "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+  "sushiswap": "https://api.thegraph.com/subgraphs/name/sushiswap/exchange",
+  "pancakeswap": "https://api.thegraph.com/subgraphs/name/shrinivasmani/pancakeswap5"
+}
+
 // const APIURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2" // uniswap
 // const APIURL = https://api.thegraph.com/subgraphs/id/0x9bde7bf4d5b13ef94373ced7c8ee0be59735a298-2 // uniswap_v3
-const APIURL = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange" // sushiswap
+// const APIURL = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange" // sushiswap
 // const APIURL = "https://api.thegraph.com/subgraphs/name/shrinivasmani/pancakeswap5" // pancakeswap
 
-const client = createClient({
-  url: APIURL
+var client = createClient({
+    url: apiExchangeUrl['uniswap']
 })
 
-function App() {
-  // const [swaps, setSwaps] = useState([])
-  const [data, setData] = useState([])
-  useEffect(() => {
-    fetchData()
-  }, [])
+class App extends Component {
 
-  // let TextFile = (response) => {
-  //   if (response && response.length!==0) {
-  //     const element = document.createElement("a");
-  //     const textFile = new Blob([[JSON.stringify(response.data.swaps)], {type: 'text/plain'}]); //pass data from localStorage API to blob
-  //     element.href = URL.createObjectURL(textFile);
-  //     element.download = "swaps.txt";
-  //     document.body.appendChild(element); 
-  //     element.click();
-  //   }
-  // }
-
-  let CsvFile = (dataset) => {
-    // console.log(dataset);
-    const data = [];
-
-    for (let i=0;i<dataset.length;i++) {
-      let record = dataset[i]
-      data.push({
-        id:record.transaction.id, 
-        swapId:record.id,
-        timestamp:record.timestamp,
-        blockNumber:record.transaction.blockNumber,   // for pancakeswap, block instead of blocknumber
-        fromCoin:record.pair.token0.symbol,
-        toCoin:record.pair.token1.symbol,
-        amount0In:record.amount0In,
-        amount1In:record.amount1In,
-        amount0Out:record.amount0Out,
-        amount1Out:record.amount1Out
-      })
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      exchange: 'uniswap'
     }
-
-    // const headers = [
-    //   { label: "id", key: "id" },
-    //   { label: "timestamp", key: "timestamp" }
-    // ];
-     
-    // const data = [
-    //   { id: "0xfea2fa9edcd1ee7e70b33cf39839251e36daa18557a0e127773b25ad512bc43d", timestamp: "1636603439" },
-    //   { id: "0xfea2fa9edcd1ee7e70b33cf39839251e36daa18557a0e127773b25ad512bc43d", timestamp: "1636603439" }
-    // ];
-     
-    setData(data);
+    this.handleExchange = this.handleExchange.bind(this)
+    this.fetchData = this.fetchData.bind(this)
   }
 
-  async function fetchData() {
+  async componentWillMount() {
+    await this.fetchData()
+  }
+
+  async fetchData() {
     let dataset = []
 
     for (let skip=initialSkip;skip<endQuery;skip+=first) {
@@ -103,58 +76,94 @@ function App() {
           }
         }
       `
-      
+
       const response = await client.query(query).toPromise();
       console.log('response:', response)
-      // console.log('swaps:', response.data.swaps);
-      // setSwaps(response.data.swaps);
-      // TextFile(response);
       dataset.push(...response.data.swaps);
     }
-    
-    CsvFile(dataset);
+
+    let data = [];
+
+      for (let i=0;i<dataset.length;i++) {
+        let record = dataset[i]
+        data.push({
+          id:record.transaction.id,
+          swapId:record.id,
+          timestamp:record.timestamp,
+          blockNumber:record.transaction.blockNumber,   // for pancakeswap, block instead of blocknumber
+          fromCoin:record.pair.token0.symbol,
+          toCoin:record.pair.token1.symbol,
+          amount0In:record.amount0In,
+          amount1In:record.amount1In,
+          amount0Out:record.amount0Out,
+          amount1Out:record.amount1Out
+        })
+      }
+      // console.log(data);
+      this.setState({data:data});
 
   }
 
-  return (
-    <div className="App">
-      <CSVLink data={data} filename={csvFilename} asyncOnClick={true} onClick={fetchData}>
-        <button type="button" class="btn btn-info" style={{margin:10+'px'}}>
-          Export to CSV
-        </button>
-      </CSVLink>
-      <table class="table table-striped">
-        <tbody>
-            <tr>
-              <th>id</th>
-              <th>timestamp</th>
-              <th>blockNumber</th>
-              <th>fromCoin</th>
-              <th>toCoin</th>
-              <th>amount0In</th>
-              <th>amount1In</th>
-              <th>amount0Out</th>
-              <th>amount1Out</th>
-            </tr>
-      {
-        data.map((datum, index) => (   
-            <tr key={datum.swapId}>
-              <td>{datum.id}</td>
-              <td>{datum.timestamp}</td>
-              <td>{datum.blockNumber}</td>
-              <td>{datum.fromCoin}</td>
-              <td>{datum.toCoin}</td>
-              <td>{datum.amount0In}</td>
-              <td>{datum.amount1In}</td>
-              <td>{datum.amount0Out}</td>
-              <td>{datum.amount1Out}</td>
-            </tr>
-        ))
-      }
-        </tbody>
-      </table>
-    </div>
-  );
+  async handleExchange(event) {
+    let a = event.target.value;
+    console.log(a);
+    this.setState({ exchange: a }, () => {
+      console.log(this.state.exchange)
+      client = createClient({
+        url: apiExchangeUrl[this.state.exchange]
+      })
+      this.fetchData()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <select className="form-select" name="exchange" id="exchange" defaultValue={this.state.exchange} onChange={this.handleExchange}>
+          <option value="uniswap">Uniswap</option>
+          <option value="sushiswap">Sushiswap</option>
+          <option value="pancakeswap">Pancakeswap</option>
+        </select> */}
+        <button value="uniswap" onClick={this.handleExchange}>uniswap</button>
+        <button value="sushiswap" onClick={this.handleExchange}>sushiswap</button>
+        <CSVLink data={this.state.data} filename={csvFilename} asyncOnClick={true} onClick={this.fetchData}>
+          <button type="button" className="btn btn-info" style={{margin:10+'px'}}>
+            Export to CSV
+          </button>
+        </CSVLink>
+        <table className="table table-striped">
+          <tbody>
+              <tr>
+                <th>id</th>
+                <th>timestamp</th>
+                <th>blockNumber</th>
+                <th>fromCoin</th>
+                <th>toCoin</th>
+                <th>amount0In</th>
+                <th>amount1In</th>
+                <th>amount0Out</th>
+                <th>amount1Out</th>
+              </tr>
+        {
+          this.state.data.map((datum, index) => (
+              <tr key={datum.swapId}>
+                <td>{datum.id}</td>
+                <td>{datum.timestamp}</td>
+                <td>{datum.blockNumber}</td>
+                <td>{datum.fromCoin}</td>
+                <td>{datum.toCoin}</td>
+                <td>{datum.amount0In}</td>
+                <td>{datum.amount1In}</td>
+                <td>{datum.amount0Out}</td>
+                <td>{datum.amount1Out}</td>
+              </tr>
+          ))
+        }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
